89476d194f3efc83c7848e4b1d3507d8362dd1bd
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-09-11 21:19:30 +0300

    feat: razor pages

---
+++ ReportGenerator.WebClient/Pages/Base/BasePageModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Octokit;
+using Octokit.Internal;
+using ReportGenerator.WebClient.Extensions;
+
+namespace ReportGenerator.WebClient.Pages.Base;
+
+public class BasePageModel : PageModel
+{
+    private static readonly ProductHeaderValue AuthInformation = new("AspNetCoreGitHubAuth");
+
+    public User GitHubUser { get; protected set; }
+
+    public string AccessToken { get; protected set; }
+
+    public virtual async Task OnGetAsync()
+    {
+        if (User.Identity is null || !User.Identity.IsAuthenticated)
+            return;
+
+        AccessToken = await HttpContext.GetAccessTokenAsync();
+
+        var credentialStore = new InMemoryCredentialStore(new Credentials(AccessToken));
+
+        var gitHubClient = new GitHubClient(AuthInformation, credentialStore);
+
+        GitHubUser = await gitHubClient.User.Current();
+    }
+}
