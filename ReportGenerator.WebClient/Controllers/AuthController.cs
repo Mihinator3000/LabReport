b9b06ff301c339674c833b2d77fb1cf440b905fc
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-09-11 21:18:43 +0300

    feat: startup settings

---
+++ ReportGenerator.WebClient/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportGenerator.WebClient.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ ReportGenerator.WebClient/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace ReportGenerator.WebClient.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGithubAuth(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection
+            .AddAuthentication(o => 
+            { 
+                o.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme; 
+                o.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme; 
+                o.DefaultChallengeScheme = "GitHub";
+            })
+            .AddCookie()
+            .AddGithubAuth(configuration);
+
+        return serviceCollection;
+    }
+}
